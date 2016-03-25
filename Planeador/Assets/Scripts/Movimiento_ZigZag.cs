3c93b85e35cd51a4cb6ece709891bae2a601fb3f
fnando1995 <fnando1995@gmaillcom>
2016-03-25 13:00:35 -0500

    pajaro rojo

---
+++ Planeador/Assets/Scripts/Movimiento_ZigZag.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento_ZigZag : MonoBehaviour {
+    public int speed = 10;
+    bool arriba = true;
+    int limite = 0;
+    int cont = 0;
+    // Use this for initialization
+    void Start () {
+	
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    
+        if (limite == cont)
+        {
+            limite = Random.Range(3, 30);
+
+            Debug.Log(limite + "- 1 -" + cont);
+            if (arriba == true)
+                arriba = false;
+            else
+                arriba = true;
+
+            cont = 0;
+
+        }
+        else
+        {
+            if (arriba == true)
+            {
+                this.transform.Translate(Vector3.up*speed*Time.deltaTime  );
+                this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            else
+            {
+                this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+                this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+
+            Debug.Log(limite + "- 2 -" + cont);
+            
+            cont++;
+        }
+
+
+        
+    }
+}
