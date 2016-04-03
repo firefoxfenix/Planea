d4b8d7360ea102ac7bacd74da5f579136097d189
firefoxfenix <christiancarriespi@Gmail.com>
2016-04-03 16:22:05 -0500

    GAMEOVER

---
+++ Planeador/Assets/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        AtributosGenerales.life = 3;
+        Application.LoadLevel("Mundo");
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+}
