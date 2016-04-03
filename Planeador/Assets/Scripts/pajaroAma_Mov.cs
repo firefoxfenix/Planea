3072643c5677ce1aa2e54b9ae518d5df602ecf9f
firefoxfenix <christiancarriespi@Gmail.com>
2016-04-03 11:12:06 -0500

    Arreglar algo

---
+++ Planeador/Assets/Scripts/pajaroAma_Mov.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pajaroAma_Mov : MonoBehaviour {
+    public GameObject Avion;
+    public int speed = 10;
+    public int speedLanzamiento = 1000;
+    bool arriba = true;
+    bool embestida = false;
+    int limite = 0;
+    int cont = 0;
+
+	void Update () {
+       if (embestida == false) {
+            if (limite == cont)
+            {
+                limite = Random.Range(3, 30);
+
+
+                if (arriba == true)
+                    arriba = false;
+                else
+                    arriba = true;
+
+                cont = 0;
+
+            }
+            else
+            {
+                if (arriba == true)
+                {
+                    this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+                    this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                else
+                {
+                    this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+                    this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+
+
+                cont++;
+            }
+        }
+
+
+        if (Vector3.Distance(new Vector3(0, this.gameObject.transform.position.y, 0), new Vector3(0, Avion.gameObject.transform.position.y, 0)) <= 1f ) //&& Vector3.Distance(new Vector3(this.gameObject.transform.position.x, 0, 0), new Vector3(Avion.gameObject.transform.position.x, 0, 0)) <= 9f)
+        {
+            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            this.transform.Translate(Vector3.left * speedLanzamiento * Time.deltaTime);
+            embestida = true;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
